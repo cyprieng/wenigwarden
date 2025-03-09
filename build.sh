26f129bf71bf3139603561f432f1b27321595f41
Cyprien Guillemot <github@guillemot.me>
2025-03-09 16:57:38 +0100

    fix: use self signed signature

--- build.sh
+++ build.sh
@@ -10,16 +10,23 @@ xcodebuild -project wenigwarden.xcodeproj \
 	-scheme wenigwarden \
 	ARCHS=arm64 \
 	-destination "platform=macOS,arch=arm64" \
-	-configuration Release CONFIGURATION_BUILD_DIR="$(pwd)/build/Release"
+	-configuration Release \
+	CODE_SIGN_IDENTITY="-" \
+	CODE_SIGNING_REQUIRED=NO \
+	CODE_SIGNING_ALLOWED=NO \
+	PROVISIONING_PROFILE="" \
+	CONFIGURATION_BUILD_DIR="$(pwd)/build/Release"
+
+codesign --force --deep --sign - "$(pwd)/build/Release/Wenigwarden.app"
 
 # Sign Sparkle
 current_dir=$(pwd)
 cd "$(pwd)/build/Release/Wenigwarden.app/Contents/Frameworks"
-codesign -f -s "$CODE_SIGN_IDENTITY" -o runtime Sparkle.framework/Versions/B/XPCServices/Installer.xpc
-codesign -f -s "$CODE_SIGN_IDENTITY" -o runtime --preserve-metadata=entitlements Sparkle.framework/Versions/B/XPCServices/Downloader.xpc
-codesign -f -s "$CODE_SIGN_IDENTITY" -o runtime Sparkle.framework/Versions/B/Autoupdate
-codesign -f -s "$CODE_SIGN_IDENTITY" -o runtime Sparkle.framework/Versions/B/Updater.app
-codesign -f -s "$CODE_SIGN_IDENTITY" -o runtime Sparkle.framework
+codesign -f -s - -o runtime Sparkle.framework/Versions/B/XPCServices/Installer.xpc
+codesign -f -s - -o runtime --preserve-metadata=entitlements Sparkle.framework/Versions/B/XPCServices/Downloader.xpc
+codesign -f -s - -o runtime Sparkle.framework/Versions/B/Autoupdate
+codesign -f -s - -o runtime Sparkle.framework/Versions/B/Updater.app
+codesign -f -s - -o runtime Sparkle.framework
 cd "$current_dir"
 
 # Create dmg
